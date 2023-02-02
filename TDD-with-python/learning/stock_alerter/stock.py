944fcdbd46ebd4463f0f3603be90b1b50fae145c
trung.leviet <trung.leviet@asnet.com.vn>
2023-02-02 15:51:03 +0700

    feat: exercises in chapter 2

--- TDD-with-python/learning/stock_alerter/stock.py
+++ TDD-with-python/learning/stock_alerter/stock.py
@@ -1,4 +1,24 @@
+import bisect
+import collections
+
+PriceEvent = collections.namedtuple("PriceEvent", ["timestamp", "price"])
+
+
 class Stock:
-  def __init__(self, ticker):
-    self.ticker = ticker
-    self.price = None
+  def __init__(self, symbol):
+    self.symbol = symbol
+    self.price_history = []
+
+  @property
+  def price(self):
+    return self.price_history[-1].price \
+      if self.price_history else None
+
+  def update(self, timestamp, price):
+    if price < 0:
+      raise ValueError("price should not be negative")
+    bisect.insort_left(self.price_history, PriceEvent(timestamp, price))
+
+  def is_increasing_trend(self):
+    return self.price_history[-3].price < \
+      self.price_history[-2].price < self.price_history[-1].price
