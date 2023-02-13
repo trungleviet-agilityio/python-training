9bfba2d62fcafaf8748bd27f42ea05f66e79b860
trung.leviet <trung.leviet@asnet.com.vn>
2023-02-13 15:35:37 +0700

    refactor: Improve code coverage

--- TDD-with-python/practice/test/test_9_1.py
+++ TDD-with-python/practice/test/test_9_1.py
@@ -5,6 +5,8 @@ with more than 20 characters (not counting whitespace).
 
 import unittest
 
+# Constant for the filename of the input file
+FILENAME = "words.txt"
 
 def print_long_words(filename):
     """
@@ -14,25 +16,40 @@ def print_long_words(filename):
     filename (str): The file name to read from.
 
     Returns:
-    list: A list of words with length greater than 20.
+    str: A comma-separated string of words with length greater than 20.
     """
-    with open('words.txt') as fin:
+    with open(filename) as fin:
         # Read all lines from the file, remove leading/trailing whitespaces and store words with length greater than 20 in a list
-        result = [word.strip() for word in fin if len(word) > 20]
-    return result
+        result = [word.strip() for word in fin if len(word.strip()) > 20]
+    return ', '.join(result)
 
 
 class TestPrintLongWords(unittest.TestCase):
+    # Test class for the print_long_words function
+
+    def setUp(self):
+        # Runs before each test method, sets up expected results
+        self.expected_short = []
+        self.expected_long = [
+            "antidisestablishmentarianism",
+            "pneumonoultramicroscopicsilicovolcanoconiosis"
+        ]
+
+    def test_file_not_found(self):
+        # Test case for the scenario where the file is not found
+        with self.assertRaises(FileNotFoundError):
+            print_long_words("missing_file.txt")
+
+    def test_file_with_short_words(self):
+        # Test case for the scenario where the file only contains short words
+        result = print_long_words(FILENAME)
+        self.assertListEqual(result, self.expected_short)
+
     def test_print_long_words(self):
-        """
-        Test if the `print_long_words` function returns the correct words.
-        """
-        words = print_long_words("words.txt")
-        # Check if the list of words is not empty
-        self.assertGreater(len(words), 0)
-        for word in words:
-            # Check if the length of each word is greater than or equal to 20
-            self.assertGreaterEqual(len(word), 20)
+        # Call print_long_words with the filename of a file that contains long words
+        result = print_long_words(FILENAME)
+        expected_result = ', '.join(self.expected_long)
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
