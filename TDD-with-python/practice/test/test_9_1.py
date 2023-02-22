1f11718517f81574636f7874ff0e4c28e9382db0
trung.leviet <trung.leviet@asnet.com.vn>
2023-02-22 09:37:00 +0700

    fix: the logic of unit tests for the function print_long_words

--- TDD-with-python/practice/test/test_9_1.py
+++ TDD-with-python/practice/test/test_9_1.py
@@ -2,9 +2,7 @@
 Write a program that reads words.txt and prints only the words
 with more than 20 characters (not counting whitespace).
 """
-
 import unittest
-from io import StringIO
 from unittest.mock import mock_open, patch
 
 
@@ -16,16 +14,16 @@ def print_long_words(filename):
     filename (str): The file name to read from.
 
     Returns:
-    str: A comma-separated string of words with length greater than 20.
+    list: A list of words with length greater than 20.
     """
     with open(filename) as fin:
         # Read all lines from the file, remove leading/trailing whitespaces and store words with length greater than 20 in a list
-        result = [word.strip() for word in fin if len(word.strip()) > 20]
-    return ', '.join(result)
+        result = [word.strip() for line in fin for word in line.split() if len(word.strip()) > 20]
+    return result
 
 
-class TestPrintLongWords(unittest.TestCase):
-    # Test class for the print_long_words function
+class TestLongWords(unittest.TestCase):
+    # Test class for the long_words function
 
     def setUp(self):
         # Runs before each test method, sets up expected results
@@ -40,22 +38,33 @@ class TestPrintLongWords(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             print_long_words("missing_file.txt")
 
+    def test_file_with_empty_content(self):
+        # Test case for the scenario where the file is empty
+        with patch('builtins.open', mock_open(read_data='')):
+            result = print_long_words('filename')
+            expected_result = []
+            self.assertEqual(result, expected_result, "Expected empty list when file is empty")
+
     def test_file_with_short_words(self):
         # Test case for the scenario where the file only contains short words
         with patch('builtins.open', mock_open(read_data='a in the the on')):
-            with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                result = print_long_words('filename')
-                expected_result = ''
-                self.assertEqual(mock_stdout.getvalue().strip(), expected_result)
-                self.assertEqual(result, self.expected_short)
+            result = print_long_words('filename')
+            expected_result = []
+            self.assertEqual(result, expected_result, "Expected empty list when file only contains short words")
 
     def test_print_long_words(self):
         # Call print_long_words with the filename of a file that contains long words
         with patch('builtins.open', mock_open(read_data='hello world antidisestablishmentarianism goodbye world pneumonoultramicroscopicsilicovolcanoconiosis')):
-            with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                print_long_words('filename')
-                expected_result = ', '.join(self.expected_long)
-                self.assertNotEqual(mock_stdout.getvalue().strip(), expected_result)
+            result = print_long_words('filename')
+            expected_result = self.expected_long
+            self.assertListEqual(result, expected_result)
+
+    def test_file_with_short_and_long_words(self):
+        # Test case for the scenario where the file contains both short and long words
+        with patch('builtins.open', mock_open(read_data='antidisestablishmentarianism a pneumonoultramicroscopicsilicovolcanoconiosis on')):
+            result = print_long_words('filename')
+            expected_result = self.expected_long
+            self.assertListEqual(result, expected_result, "Expected only long words in the file")
 
 
 if __name__ == '__main__':
