e6732360b30fad39ee887285a80fe81164aa60f9
trungle-agilityio <trung.leviet@asnet.com.vn>
2023-09-06 16:54:35 +0700

    feat: implement token api

--- django-learning/app/user/tests/test_user_api.py
+++ django-learning/app/user/tests/test_user_api.py
@@ -10,6 +10,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
@@ -64,54 +65,52 @@ class PublicUserApiTests(TestCase):
         ).exists()
         self.assertFalse(user_exists)
 
-    # def test_create_token_for_user(self):
-    #     """Test generates token for valid credentials."""
-    #     user_details = {
-    #         'name': 'Test Name',
-    #         'email': 'test@example.com',
-    #         'password': 'test-user-password123',
-    #     }
-    #     create_user(**user_details)
+    def test_create_token_for_user(self):
+        """Test generates token for valid credentials."""
+        user_details = {
+            'name': 'Test Name',
+            'email': 'test@example.com',
+            'password': 'test-user-password123',
+        }
+        create_user(**user_details)
 
-    #     payload = {
-    #         'email': user_details['email'],
-    #         'password': user_details['password'],
-    #     }
-    #     res = self.client.post(TOKEN_URL, payload)
+        payload = {
+            'email': user_details['email'],
+            'password': user_details['password'],
+        }
+        res = self.client.post(TOKEN_URL, payload)
 
-    #     self.assertIn('token', res.data)
-    #     self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
-    # def test_create_token_bad_credentials(self):
-    #     """Test returns error if credentials invalid."""
-    #     create_user(email='test@example.com', password='goodpass')
+    def test_create_token_bad_credentials(self):
+        """Test returns error if credentials invalid."""
+        create_user(email='test@example.com', password='goodpass')
 
-    #     payload = {'email': 'test@example.com', 'password': 'badpass'}
-    #     res = self.client.post(TOKEN_URL, payload)
+        payload = {'email': 'test@example.com', 'password': 'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
 
-    #     self.assertNotIn('token', res.data)
-    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_create_token_email_not_found(self):
-    #     """Test error returned if user not found for given email."""
-    #     payload = {'email': 'test@example.com', 'password': 'pass123'}
-    #     res = self.client.post(TOKEN_URL, payload)
+    def test_create_token_email_not_found(self):
+        """Test error returned if user not found for given email."""
+        payload = {'email': 'test@example.com', 'password': 'pass123'}
+        res = self.client.post(TOKEN_URL, payload)
 
-    #     self.assertNotIn('token', res.data)
-    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_create_token_blank_password(self):
-    #     """Test posting a blank password returns an error."""
-    #     payload = {'email': 'test@example.com', 'password': ''}
-    #     res = self.client.post(TOKEN_URL, payload)
+    def test_create_token_blank_password(self):
+        """Test posting a blank password returns an error."""
+        payload = {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
 
-    #     self.assertNotIn('token', res.data)
-    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_retrieve_user_unauthorized(self):
-    #     """Test authentication is required for users."""
-    #     res = self.client.get(ME_URL)
+    def test_retrieve_user_unauthorized(self):
+        """Test authentication is required for users."""
+        res = self.client.get(ME_URL)
 
-    #     self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-
-
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
