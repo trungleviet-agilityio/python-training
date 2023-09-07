071a6401362453a5033c29bec9e00fb9c5c1e167
trungle-agilityio <trung.leviet@asnet.com.vn>
2023-09-07 11:22:36 +0700

    feat: write tests and implement update tag api

--- django-learning/app/recipe/views.py
+++ django-learning/app/recipe/views.py
@@ -41,7 +41,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -51,3 +53,4 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
