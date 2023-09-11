ec78c908e80235d4c928f11994b5862be86163db
trungle-agilityio <trung.leviet@asnet.com.vn>
2023-09-11 16:02:01 +0700

    remove: django_learning project to setup the new one

--- django_learning/form_learning/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FormLearningConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'form_learning'
--- django_learning/form_learning/urls.py
+++
@@ -1,8 +0,0 @@
-# products/urls.py
-from django.urls import path
-from form_learning import views
-
-urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('add/', views.add_product, name='add_product'),
-]
