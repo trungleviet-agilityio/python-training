35c7c3ba0c3770cafa23006bd9e872654892762a
trungle-agilityio <trung.leviet@asnet.com.vn>
2023-09-12 10:30:34 +0700

    feat: write tests and implement Department model

--- employee-project/app/core/tests/test_model.py
+++ employee-project/app/core/tests/test_model.py
@@ -13,22 +13,33 @@ def create_user(email='user@example.com', password='testpass123'):
 class ModelTests(TestCase):
     """Test models"""
 
-    def test_create_superuser(self):
-        """Test creating a superuser"""
-        user = get_user_model().objects.create_superuser(
-            'test@example.com',
-            'test123',
+    def setUp(self):
+        # Create a superuser
+        self.user = get_user_model().objects.create_superuser(
+            username='testuser',
+            email='test@example.com',
+            password='test123',
         )
 
-        self.assertTrue(user.is_superuser)
-        self.assertTrue(user.is_staff)
+        # Create departments
+        self.department_hr = models.Department.objects.create(
+            name="Human Resources",
+            description="This is the Human Resources department."
+        )
+        self.department_marketing = models.Department.objects.create(
+            name="Marketing",
+            description="This is the Marketing department."
+        )
 
-    def setUp(self):
-        # Create test department
-        self.department_hr = Department.objects.create(name="Human Resources")
-        self.department_marketing = Department.objects.create(name="Marketing")
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        self.assertTrue(self.user.is_superuser)
+        self.assertTrue(self.user.is_staff)
 
     def test_create_department(self):
         """Test creating a department is successful"""
         self.assertEqual(self.department_hr.name, "Human Resources")
+        self.assertEqual(self.department_hr.description, "This is the Human Resources department.")
+
         self.assertEqual(self.department_marketing.name, "Marketing")
+        self.assertEqual(self.department_marketing.description, "This is the Marketing department.")
