b2f917bd4c84fa1d727e4ac57b5494f769e5af3d
trungle-agilityio <trung.leviet@asnet.com.vn>
2023-09-12 10:49:51 +0700

    feat: write tests and implement contact model

--- employee-project/app/core/tests/test_model.py
+++ employee-project/app/core/tests/test_model.py
@@ -31,6 +31,23 @@ class ModelTests(TestCase):
             description="This is the Marketing department."
         )
 
+        # Create test contacts
+        self.contact1 = models.Contact.objects.create(
+            street_address="123 Main St", 
+            city="New York", 
+            state="NY", 
+            postal_code="10001",
+            phone_number="555-123-4567"
+        )
+        self.contact2 = models.Contact.objects.create(
+            street_address="456 Elm St", 
+            city="Los Angeles", 
+            state="CA", 
+            postal_code="90001",
+            phone_number="555-987-6543"
+        )
+
+
     def test_create_superuser(self):
         """Test creating a superuser"""
         self.assertTrue(self.user.is_superuser)
@@ -43,3 +60,17 @@ class ModelTests(TestCase):
 
         self.assertEqual(self.department_marketing.name, "Marketing")
         self.assertEqual(self.department_marketing.description, "This is the Marketing department.")
+
+    def test_create_contact(self):
+        """Test creating a contact is successful"""
+        self.assertEqual(self.contact1.street_address, "123 Main St")
+        self.assertEqual(self.contact1.city, "New York")
+        self.assertEqual(self.contact1.state, "NY")
+        self.assertEqual(self.contact1.postal_code, "10001")
+        self.assertEqual(self.contact1.phone_number, "555-123-4567")
+
+        self.assertEqual(self.contact2.street_address, "456 Elm St")
+        self.assertEqual(self.contact2.city, "Los Angeles")
+        self.assertEqual(self.contact2.state, "CA")
+        self.assertEqual(self.contact2.postal_code, "90001")
+        self.assertEqual(self.contact2.phone_number, "555-987-6543")
