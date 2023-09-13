be909b2f42321835be9243eff09cfbe84b9682d5
trungle-agilityio <trung.leviet@asnet.com.vn>
2023-09-13 14:50:49 +0700

    feat: added templates for each model and dummy data for rendering in views

--- employee-project/app/employee/urls.py
+++ employee-project/app/employee/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("employees", views.employees, name="employees-page"),
+    path('', views.home, name='home'),
+    path('employees/', views.employee_list, name='employee-list'),
+    path('contacts/', views.contact_list, name='contact-list'),
+    path('departments/', views.department_list, name='department-list'),
+    path('projects/', views.project_list, name='project-list'),
 ]
--- employee-project/app/employee/views.py
+++ employee-project/app/employee/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'employee/home.html')
 
 
-def starting_page(request):
-    return render(request, "employee/index.html")
+def employee_list(request):
+    employees = [
+        {"first_name": "John", "last_name": "Doe", "department": "HR"},
+        {"first_name": "Jane", "last_name": "Smith", "department": "Engineering"},
+        {"first_name": "Alice", "last_name": "Johnson", "department": "Sales"},
+    ]
+    return render(request, 'employee/employee_list.html', {'employees': employees})
 
 
-def employees(request):
-    pass
+def contact_list(request):
+    contacts = [
+        {"street_address": "123 Main St", "city": "New York", "state": "NY", "postal_code": "10001", "phone_number": "123-456-7890"},
+        {"street_address": "456 Elm St", "city": "Los Angeles", "state": "CA", "postal_code": "90001", "phone_number": "987-654-3210"},
+        {"street_address": "789 Oak St", "city": "Chicago", "state": "IL", "postal_code": "60601", "phone_number": "555-123-4567"},
+    ]    
+    return render(request, 'employee/contact_list.html', {'contacts': contacts})
+
+
+def department_list(request):
+    departments = [
+        {"name": "HR", "description": "Human Resources"},
+        {"name": "Engineering", "description": "Software Development"},
+        {"name": "Sales", "description": "Sales and Marketing"},
+    ]    
+    return render(request, 'employee/department_list.html', {'departments': departments})
+
+
+def project_list(request):
+    projects = [
+        {"title": "Project A"},
+        {"title": "Project B"},
+        {"title": "Project C"},
+    ]    
+    return render(request, 'employee/project_list.html', {'projects': projects})
